0ca54fcbb6fb72c517d028424e598afff5e332f8
Callum Seabrook <callum.seabrook@prevarinite.com>
2023-01-11 21:13:51 +0000

    Added time variable to freeze command

--- src/main/java/com/github/sirblobman/freeze/command/SubCommandFreezePlayer.java
+++ src/main/java/com/github/sirblobman/freeze/command/SubCommandFreezePlayer.java
@@ -1,9 +1,12 @@
 package com.github.sirblobman.freeze.command;
 
+import com.github.sirblobman.api.language.ComplexReplacer;
+import java.math.BigInteger;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -49,6 +52,25 @@ public class SubCommandFreezePlayer extends FreezeCommand {
         FreezeManager freezeManager = getFreezeManager();
         if (freezeManager.isFrozen(target)) {
             sendMessage(sender, "error.currently-frozen", targetNameReplacer);
+            return true;
+        }
+
+        if (args.length > 1) {
+            final BigInteger seconds = parseInteger(sender, args[1]);
+            if (seconds == null) {
+                return true;
+            }
+            final Replacer timeReplacer = getReplacer("{duration}", String.valueOf(seconds.longValue()));
+
+            freezeManager.setFrozen(target, true);
+            sendMessage(sender, "freeze-time", new ComplexReplacer(targetNameReplacer, timeReplacer));
+
+            Bukkit.getScheduler().runTaskLater(getPlugin(), () -> {
+                if (freezeManager.isFrozen(target)) {
+                    freezeManager.setFrozen(target, false);
+                    sendMessage(sender, "unfreeze", targetNameReplacer);
+                }
+            }, seconds.longValue() * 20L);
             return true;
         }
 
